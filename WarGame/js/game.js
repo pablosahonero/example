75f8b3e07f2191211e3bda8807b32df0e2ee2186
Samuel Vargas <varguitas0911@gmail.com>
2015-03-30 16:01:25 -0400

    Adding game class

--- WarGame/js/game.js
+++ WarGame/js/game.js
@@ -1,13 +1,25 @@
 /**
  * Created by Samuel Vargas on 3/24/2015.
  */
-    /**/
-var Game = function (name)
+var Game = function (name, players, size)// be careful with the size, size = 10
 {
-    var _Name = name;
-    var _Boards = Array();
+    var _name = name;
+    var _boards = Array();
+    var _players = players;
+    var _size = size;
 
     this.startGame = function(){
+        for(var i = 0; i < _players; i++){
+            var board = new Board(i, _size);
+            board.initializeBoard();
+            _boards.push(board);
 
+        };
+    };
+
+    this.displayGame = function(){
+        for(var i = 0; i < _boards.length; i++){
+            _boards[i].displayBoard();
+        };
     };
 };
