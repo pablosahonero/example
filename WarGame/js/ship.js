fff6775cebb66fd125f3d556e3734c004a55dbc6
Pablo Sahonero <pablo.sahonero.2407@gmail.com>
2015-03-30 17:12:47 -0400

    Comments about Ship class made by Pablo Sahonero and Samuel Vargas

--- WarGame/js/ship.js
+++ WarGame/js/ship.js
@@ -2,15 +2,40 @@
  * Created by Pablo Sahonero on 3/24/2015.
  */
 var Ship = function(){
+    /**
+     * Represents the location of Ship based on Positions
+     * @type {Array}
+     */
     var _location;
+    /**
+     * Represents the Ship orientation in the Board
+     * @type {number}
+     */
     var _orientation;
+
+    /**
+     * Represents first position of the Ship
+     * @type {Position}
+     */
     var _startPosition;
+
+    /**
+     * Represents last position of the Ship
+     * @type {Position}
+     */
     var _endPosition;
 
+    /**
+     * Returns the Ship's location
+     * @return {Array}
+     */
     this.getLocation = function(){
         return _location;
     };
 
+    /**
+     * Initializes the Ship orientation and location
+     */
     this.initializeShip = function(){
         //length from 1 block up to 4 (should be a constant)
         _orientation = Math.floor((Math.random() * 2) + 1);
@@ -18,11 +43,14 @@ var Ship = function(){
         this.setLocation(Math.floor((Math.random() * CONST.get('MAX_SHIP_SIZE')) + 1));
     };
 
+    /**
+     * Sets the Ship's location based on an Array of Positions
+     */
     this.setLocation = function(l){
         var length = l;
         if(_location.length == 0){
 
-            var position = new Position(Math.floor(Math.random() * CONST.get('BOARD_SIZE_MIN'))+0, Math.floor(Math.random() * CONST.get('BOARD_SIZE_MIN'))+0);
+            var position = new Position(Math.floor(Math.random() * CONST.get('BOARD_SIZE'))+0, Math.floor(Math.random() * CONST.get('BOARD_SIZE'))+0);
             _location.push(position);
             _startPosition = position;
             _endPosition = position;
@@ -30,7 +58,7 @@ var Ship = function(){
         };
 
         if(length > 0){
-            if(_orientation == 1)//vertical
+            if(_orientation == CONST.get('VERTICAL'))//vertical
             {
                 if(_startPosition.getY()-1 >= 0){
 
@@ -39,7 +67,7 @@ var Ship = function(){
                     _location.push(position);
                     _startPosition = position;
                 }else{
-                    if(_endPosition.getY()+1 <= CONST.get('BOARD_SIZE_MIN')){
+                    if(_endPosition.getY()+1 <= CONST.get('BOARD_SIZE')){
                         var position = new Position(_endPosition.getX(), _endPosition.getY()+1);
 
                         _location.push(position);
@@ -54,7 +82,7 @@ var Ship = function(){
                     _location.push(position);
                     _startPosition = position;
                 }else{
-                    if(_endPosition.getX()+1<= CONST.get('BOARD_SIZE_MIN')){
+                    if(_endPosition.getX()+1<= CONST.get('BOARD_SIZE')){
                         var position = new Position(_endPosition.getX()+1, _endPosition.getY());
 
                         _location.push(position);
@@ -66,6 +94,9 @@ var Ship = function(){
         };
     };
 
+    /**
+     * Displays the location of the Ship based in Positions
+     */
     this.displayLocation = function(){
         for (var i = 0; i < _location.length; i++) {
             console.log("cell " + i +   ": " + "x = "+_location[i].getX() + "; y = " + _location[i].getY());
