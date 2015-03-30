d11a39a85014b0fb8c6a76487a917f2e722881d0
Administrator <paolocesar2004@gmail.com>
2015-03-30 15:54:37 -0400

    Last Changes by Paulo Ormachea and Milenca Ventura

---
+++ WarGame/js/player.js
@@ -0,0 +1,28 @@
+var Player = function(id){
+    var _id = id;
+    var _score = 0;
+    var _ships = new Array();
+
+    this.getShips = function(){
+        return _ships;
+    };
+
+    this.createShips = function(){
+        for(var i = 0; i < 4; i++){
+            var ship = new Ship();
+            ship.initializeShip();
+            _ships.push(ship);
+        };
+    };
+
+    this.displayPlayer = function(){
+        console.log('Player ' + _id + ' : ' + _score +' pts');
+    };
+
+    this.displayShips = function(){
+        for(var i = 0; i < 4; i++){
+            console.log("Ship " + i + " :");
+            _ships[i].displayLocation();
+        };
+    };
+};
