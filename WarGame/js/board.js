5cb40f94f15a3cb49bd312f51cb83ca67eede7ab
Samuel Vargas <varguitas0911@gmail.com>
2015-03-30 16:56:56 -0400

    Adding comments in the Game, Board class

--- WarGame/js/board.js
+++ WarGame/js/board.js
@@ -1,23 +1,63 @@
 /**
  * Created by Pablo Sahonero on 3/23/2015.
  */
+
+/**
+ * This class represents a Boards that is being used in the game
+ * @param represents the Board identification
+ * @param represents the Board size
+ */
 var Board = function(id, size)// be careful with the size parameter
 {
+
+    /**
+     * identification of the Game
+     * @type {number}
+     */
     var _id = id;
+
+    /**
+     * the Player associated to the current Board
+     * @type {Player}
+     */
     var _player = new Player(_id);
+
+    /**
+     * The length of the board
+     * @type {number}
+     */
     var _size = size;
+
+    /**
+     * the matrix that represents the Positions of the Board
+     * @type {Array}
+     */
     var _matrix = new Array();
 
+
+    /**
+     * This function returns the Board's Matrix
+     * @return {Array}
+     */
     this.getMatrix = function(){
         return _matrix;
     };
 
+    /**
+     * This function create the Ships of the Player, initializes the matrix and
+     * locates the Ships on the Board
+     *
+     */
     this.initializeBoard = function(){
         _player.createShips();
         this.initializeMatrix();
         this.locateShips(_player.getShips());
     };
 
+    /**
+     * This function initializes all the positions of the matrix to FREE_SPACE
+     *
+     */
     this.initializeMatrix = function(){
         for(var i = 0; i < size; i++){
             _matrix.push(new Array());
@@ -27,6 +67,10 @@ var Board = function(id, size)// be careful with the size parameter
         };
     };
 
+    /**
+     * This function locates the Ship in the Board
+     *
+     */
     this.locateShips = function(array){
         var ships = array;
         for(var i = 0; i < ships.length; i++){
@@ -38,6 +82,10 @@ var Board = function(id, size)// be careful with the size parameter
         };
     };
 
+    /**
+     * This function display the Board, the Ship and the Shots
+     *
+     */
     this.displayBoard = function(){
         _player.displayPlayer();
         for(var i = 0; i < _size; i++){
--- WarGame/js/game.js
+++ WarGame/js/game.js
@@ -1,22 +1,54 @@
 /**
  * Created by Samuel Vargas on 3/24/2015.
  */
+
+/**
+ * Represents the current Game
+ * @param name represents the name of the Game
+ * @param players represents the number of players
+ * @param represents the size of the Player's Board
+ */
 var Game = function (name, players, size)// be careful with the size, size = 10
 {
+    /**
+     * Name of the Game
+     * @type {string}
+     */
     var _name = name;
+    /**
+     * Array of the Boards that are going to be used in the game
+     * @type {Array{Board}}
+     */
     var _boards = Array();
+    /**
+     * Number of players in the Game
+     * @type {number}
+     */
     var _players = players;
+    /**
+     * Size of the Boards that are going to be used in the game
+     * @type {number}
+     */
     var _size = size;
 
+    /**
+     * This function initializes the boards to be used during the game.
+     */
     this.startGame = function(){
         for(var i = 0; i < _players; i++){
+            /**
+             * Object Board to be used in the game
+             * @type {Board}
+             */
             var board = new Board(i, _size);
             board.initializeBoard();
             _boards.push(board);
-
         };
     };
 
+    /**
+     * This function displays all the Boards involved in the game
+     */
     this.displayGame = function(){
         for(var i = 0; i < _boards.length; i++){
             _boards[i].displayBoard();
