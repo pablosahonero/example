a44ffc86f52e0504d72e4bbfb96eee006b9bea27
Milenca Ventura <Milenca.VenturaCanaviri@fundacion-jala.org>
2015-03-31 20:46:29 -0400

    shot

---
+++ WarGame/js/shot.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Milenca Ventura on 3/31/2015.
+ */
+/**
+ * this js represent the status and the number of shooting for each player.
+ */
+var shot = function() {
+    /**
+     * Represents the shot and the state(Eg. )
+     * @type {Array}
+     */
+    var _state;
+    /**
+     * Represents the status of the ship in the board
+     * @type {number}
+     */
+    var _number;
+
+    /**
+     * Represents the number of the shot
+     * @type {Position}
+     */
+    this.getState = function () {
+        return _location;
+    };
+    /**
+     * Initializes the status of the Shot
+     * */
+
+ };
