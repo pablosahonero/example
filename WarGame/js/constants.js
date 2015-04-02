52ac2b9b4fcce566e651f1c518b2e5b2cfc02163
Samuel Vargas <varguitas0911@gmail.com>
2015-04-02 15:38:26 -0400

    upload the status of constants for the class Shot

--- WarGame/js/constants.js
+++ WarGame/js/constants.js
@@ -3,43 +3,47 @@
  */
 var CONST = (function() {
     var private = {
-/**
- * This varialbe is for the MAX size of the Ships
- */
+        /**
+         * This variable is for the MAX size of the Ships
+         */
         'MAX_SHIP_SIZE': 4,
-/**
- * This variable is for the total amount of ships by player, that measn every player have 10 ships
- */
+        /**
+         * This variable is for the total amount of ships by player, that measn every player have 10 ships
+         */
         'SHIPS_NUMBER': 10,
-/**
- * This variable is for the size of the game board
- */
+        /**
+         * This variable is for the size of the game board
+         */
         'BOARD_SIZE': 10,
-/**
- * This variable is for the turs of every player. E.G for 3 player exists 60 turn 20 for every player
- */
+        /**
+         * This variable is for the turns of every player. E.G for 3 player exists 60 turn 20 for every player
+         */
         'GAME_TURNS': 20,
-/**
- * Thi variables are for the game board, in the board for drawing the game a "0" value represents a FREE SPACE,
- * "1" value represents a SHIP and "2" represents a Shots
- */
+        /**
+         * Thi variables are for the game board, in the board for drawing the game a "0" value represents a FREE SPACE,
+         * "1" value represents a SHIP and "2" represents a Shots
+         */
         'FREE_SPACE': 0,
         'SHIP_SPACE': 1,
         'SHOT_SPACE': 2,
-/**
- * This functions are for define the orientation of the ships
- *
- */
+        /**
+         * This functions are for define the orientation of the ships
+         *
+         */
         'VERTICAL': 1,
         'HORIZONTAL': 2,
-/**
-*This variable is for change the turns for each player
- *
-*/
+        /**
+         *This variable is for change the turns for each player
+         *
+         */
         'CONTROL_TURNS': true,
+
         'NONE': 0,
         'HIT': 1,
-        'FAIL': 2
+        'FAIL': 2,
+
+        'SAFE': 0,
+        'DAMAGED': 1
     };
 
     return {
