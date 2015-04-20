43200fe353b4ea1f32269eca57b56edaf10b6b4c
Pablo Sahonero <pablo.sahonero.2407@gmail.com>
2015-04-20 17:12:09 -0400

    Creation of SpecRunner for Jasmine Tests

---
+++ WarGame/spec/WarGameSpec.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Milenca Ventura on 4/2/2015.
+ */
+describe('War Game', function(){
+
+    describe('Constants', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('Position', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('Ship', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('Shot', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('Board', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('Players', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+
+    describe('Game', function(){
+        it('Test 1', function(){
+            expect(true).toBe(true);
+        });
+    });
+});
--- WarGame/spec/gameSpec.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Milenca Ventura on 4/2/2015.
- */
